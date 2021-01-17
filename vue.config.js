8991df4825e0edb31a72593510e6747a0df69d67
Bestian Tang 小巴 <bestian@gmail.com>
2021-01-18 07:34:50 +0800

    debug pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 module.exports = {
+  css: { sourceMap: true },
+  productionSourceMap: true,
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+      maximumFileSizeToCacheInBytes: 500000,  
+      exclude: [/\.gitkeep/, /.(?:png|jpg|jpeg|svg|txt|json)$/],
+      runtimeCaching: [
+        {
+          // Match any request ends with .png, .jpg, .jpeg or .svg.
+          urlPattern: /.(?:png|jpg|jpeg|svg|txt|json)$/,
+          // Apply a cache-first strategy.
+          handler: "CacheFirst",
+          options: {
+            // Use a custom cache name.
+            cacheName: "images"
+          }
+        }
+      ]
+    }
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
     : '/'
